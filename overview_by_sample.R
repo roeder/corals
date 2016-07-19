b22a72461aad51e3165f0cb18cda1b2fca721db5
Timo Roeder <roeder.t17@gmail.com>
2016-07-19 12:14:18 +0200

    implement ITS2 output

--- overview_by_sample.R
+++ overview_by_sample.R
@@ -6,19 +6,19 @@ library(tidyr)
 its1_raw <- read_excel('Linett_total_ITS1.xlsx')
 its2_raw <- read_excel('Linett_total_ITS2.xlsx')
 
-# Fixed names!
+# ITS1: Fixed names are loaded from separate spreadsheet!
 samples_its1 <- read_excel('ITS1 miseq data with sites.xlsx')$Samples
 colnames(its1_raw)[28:197] <- samples_its1
 
-# names are not fixed - there are duplicate names (PCR neg)
+# ITS2: Fix names by appending letter indices a, b, c & d to duplicate names
+# The duplicate names are PCR neg_1 and PCR neg_2
+colnames(its2_raw)[grep('neg_1', colnames(its2_raw), ignore.case = T)] <- 
+  paste0(colnames(its2_raw)[grep('neg_1', colnames(its2_raw), ignore.case = T)], '_', letters[1:4])
+
+colnames(its2_raw)[grep('neg_2', colnames(its2_raw), ignore.case = T)] <- 
+  paste0(colnames(its2_raw)[grep('neg_2', colnames(its2_raw), ignore.case = T)], '_', letters[1:4])
+
 samples_its2 <- colnames(its2_raw)[28:197]
-
-samples_its2[grep('neg_1', samples_its2, ignore.case = T)] <- 
-  paste0(samples_its2[grep('neg_1', samples_its2, ignore.case = T)], '_', letters[1:4])
-
-samples_its2[grep('neg_2', samples_its2, ignore.case = T)] <- 
-  paste0(samples_its2[grep('neg_2', samples_its2, ignore.case = T)], '_', letters[1:4])
-
 
 # ITS 1 process
 its1_data <- its1_raw %>% 
@@ -71,3 +71,29 @@ its2_sample_overview <- its2_data %>%
 its2_data <- its2_data %>% 
   filter(abundance > 0) %>% 
   arrange(sample, desc(abundance))
+
+file.create('ITS2_by_sample.md')
+
+for (i in 1:nrow(its2_sample_overview)) {
+  cat(paste0('### ', samples_its2[i], '\n\n'), file = 'ITS2_by_sample.md', append = T)
+  
+  if (its2_sample_overview[its2_sample_overview$sample == samples_its2[i], 2] == 0) {
+    cat('no hits!\n\n', file = 'ITS2_by_sample.md', append = T)
+    next()
+  }
+  
+  cat('OTU | Info | Abundance\n--- | --- | ---\n', file = 'ITS2_by_sample.md', append = T)
+  
+  sample_hits <- filter(its2_data, sample == samples_its2[i])
+  
+  for (j in 1:nrow(sample_hits)) {
+    output_string <- paste(sample_hits$OTU[j], 
+                           substr(sample_hits$header[j], 1, 40), 
+                           sample_hits$abundance[j], 
+                           sep = ' | ')
+    cat(paste0(output_string, '\n'), file = 'ITS2_by_sample.md', append = T)
+  }
+  
+  
+  cat('\n', file = 'ITS2_by_sample.md', append = T)
+}
