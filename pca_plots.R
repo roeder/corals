0b8d7a9a1a91710335a7f29dfd7d51dc5a1c1765
Timo Roeder <roeder.t17@gmail.com>
2016-08-10 17:42:01 +0200

    implement matrix prep

---
+++ pca_plots.R
@@ -0,0 +1,44 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+### Read in data ---------
+its1_raw <- read_excel('Linett_total_ITS1.xlsx')
+its2_raw <- read_excel('Linett_total_ITS2.xlsx')
+
+sample_info <- read_excel('ITS1 miseq data with sites.xlsx')[, 2:3]
+colnames(sample_info) <- c('diseased', 'sample')
+
+# ITS1: Fixed names are loaded from separate spreadsheet!
+samples_its1 <- sample_info$sample
+
+# ITS2: Fix duplicate PCR neg names by appending new indices in order of
+# appearance
+colnames(its2_raw)[grep('PCR', colnames(its2_raw), ignore.case = T)] <- 
+  paste0('PCR_neg_', 1:10)
+
+samples_its2 <- colnames(its2_raw)[28:197]
+
+sample_info <- rbind(sample_info, c("-", 'PCR_neg_10'))
+sample_info$diseased[is.na(sample_info$diseased)] <- '-'
+
+source('normalise_sample_names.R')
+
+colnames(its1_raw)[28:197] <- samples_its1
+colnames(its2_raw)[28:197] <- samples_its2
+
+dataset_split <- grep('(', sample_info$sample, fixed = T)[1]
+sample_info$dataset <- c(rep('original', dataset_split - 1), 
+                         rep('extra', (nrow(sample_info) - dataset_split + 1)))
+
+control_samples <- c(grep('pcr_neg_', sample_info$sample, fixed = T, value = T),
+                     grep('ex_con_', sample_info$sample, fixed = T, value = T))
+
+### Transpose ITS1 data ---------
+its1_df <- its1_raw %>% 
+  select(OTU, one_of(setdiff(samples_its1, control_samples))) %>% 
+  arrange(OTU)
+
+its1_matrix <- t(data.matrix(its1_df[, -1]))
+
+colnames(its1_matrix) <- paste0("OTU_", its1_df$OTU)
