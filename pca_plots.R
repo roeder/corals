cd5a21e0b59216f83646655383ac367931246ecb
Timo Roeder <roeder.t17@gmail.com>
2016-08-11 13:32:34 +0200

    implement the four PCAs

--- pca_plots.R
+++ pca_plots.R
@@ -31,14 +31,74 @@ dataset_split <- grep('(', sample_info$sample, fixed = T)[1]
 sample_info$dataset <- c(rep('original', dataset_split - 1), 
                          rep('extra', (nrow(sample_info) - dataset_split + 1)))
 
+original_samples <- sample_info$sample[1:dataset_split - 1]
+extra_samples <- sample_info$sample[dataset_split:nrow(sample_info)]
+
 control_samples <- c(grep('pcr_neg_', sample_info$sample, fixed = T, value = T),
                      grep('ex_con_', sample_info$sample, fixed = T, value = T))
 
-### Transpose ITS1 data ---------
-its1_df <- its1_raw %>% 
-  select(OTU, one_of(setdiff(samples_its1, control_samples))) %>% 
+### PCA 1: ITS1 & original data ---------
+df1 <- its1_raw %>% 
+  select(OTU, one_of(setdiff(intersect(samples_its1, original_samples), 
+                             control_samples))) %>% 
   arrange(OTU)
 
-its1_matrix <- t(data.matrix(its1_df[, -1]))
+matrix1 <- t(data.matrix(df1[, -1]))
 
-colnames(its1_matrix) <- paste0("OTU_", its1_df$OTU)
+colnames(matrix1) <- paste0("OTU_", df1$OTU)
+
+pca1 <- prcomp(matrix1, center = F)
+
+pca_var1 <- pca1$sdev ^ 2
+
+cum_var1 <- cumsum(pca_var1 / sum(pca_var1))
+
+### PCA 2: ITS1 & extra data ---------
+df2 <- its1_raw %>% 
+  select(OTU, one_of(setdiff(intersect(samples_its1, extra_samples), 
+                             control_samples))) %>% 
+  arrange(OTU)
+
+matrix2 <- t(data.matrix(df2[, -1]))
+
+colnames(matrix2) <- paste0("OTU_", df2$OTU)
+
+pca2 <- prcomp(matrix2, center = F)
+
+pca_var2 <- pca2$sdev ^ 2
+
+cum_var2 <- cumsum(pca_var2 / sum(pca_var2))
+
+### PCA 3: ITS2 & original data ---------
+df3 <- its2_raw %>% 
+  select(OTU, one_of(setdiff(intersect(samples_its2, original_samples), 
+                             control_samples))) %>% 
+  arrange(OTU)
+
+matrix3 <- t(data.matrix(df3[, -1]))
+
+colnames(matrix3) <- paste0("OTU_", df3$OTU)
+
+pca3 <- prcomp(matrix3, center = F)
+
+pca_var3 <- pca3$sdev ^ 2
+
+cum_var3 <- cumsum(pca_var3 / sum(pca_var3))
+
+### PCA 4: ITS2 & extra data ---------
+df4 <- its2_raw %>% 
+  select(OTU, one_of(setdiff(intersect(samples_its2, extra_samples), 
+                             control_samples))) %>% 
+  arrange(OTU)
+
+matrix4 <- t(data.matrix(df4[, -1]))
+
+colnames(matrix4) <- paste0("OTU_", df4$OTU)
+
+pca4 <- prcomp(matrix4, center = F)
+
+pca_var4 <- pca4$sdev ^ 2
+
+cum_var4 <- cumsum(pca_var4 / sum(pca_var4))
+
+### Plot prep -------------
