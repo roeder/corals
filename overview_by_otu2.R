13de31a3dd9292a3e75335c2c42a7a54c05ef451
Timo Roeder <roeder.t17@gmail.com>
2016-08-22 17:59:23 +0200

    new counting by genus

---
+++ overview_by_otu2.R
@@ -0,0 +1,128 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+### Read in data ---------
+its1_raw <- read_excel('input/Linett_total_ITS1.xlsx')
+its2_raw <- read_excel('input/Linett_total_ITS2.xlsx')
+
+sample_info <- read_excel('input/ITS1 miseq data with sites.xlsx')[, 2:3]
+colnames(sample_info) <- c('diseased', 'sample')
+
+# ITS1: Fixed names are loaded from separate spreadsheet!
+samples_its1 <- sample_info$sample
+
+# ITS2: Fix duplicate PCR neg names by appending new indices in order of
+# appearance
+colnames(its2_raw)[grep('PCR', colnames(its2_raw), ignore.case = T)] <- 
+  paste0('PCR_neg_', 1:10)
+
+samples_its2 <- colnames(its2_raw)[28:197]
+
+sample_info <- rbind(sample_info, c("-", 'PCR_neg_10'))
+sample_info$diseased[is.na(sample_info$diseased)] <- '-'
+
+source('normalise_sample_names.R')
+
+colnames(its1_raw)[28:197] <- samples_its1
+colnames(its2_raw)[28:197] <- samples_its2
+
+dataset_split <- grep('(', sample_info$sample, fixed = T)[1]
+sample_info$dataset <- c(rep('original', dataset_split - 1), 
+                         rep('extra', (nrow(sample_info) - dataset_split + 1)))
+
+group_overview <- sample_info %>% 
+  group_by(dataset, diseased) %>% 
+  summarise(total_samples = n())
+
+group_overview$status <- rep(c('not assigned', 'healthy', 'sick'), 2)
+
+### Turn ITS1 into long tables ---------
+its1_long <- its1_raw %>% 
+  select(OTU, header, one_of(samples_its1)) %>% 
+  gather(sample, reads, one_of(samples_its1)) %>% 
+  left_join(sample_info, by = 'sample') %>% 
+  filter(diseased != '-',
+         reads > 1) 
+
+its1_genus <- its1_long %>% 
+  mutate(genus = word(header, end = 1)) %>% 
+  group_by(sample, genus) %>% 
+  mutate(occurence = row_number()) %>% 
+  filter(occurence == 1) %>% 
+  left_join(group_overview[1:3, c(2, 4)], by = 'diseased') %>% 
+  ungroup()
+
+genus1 <- its1_genus %>% 
+  filter(dataset == 'original') %>% 
+  group_by(genus, status) %>% 
+  summarise(freq = n()) %>% 
+  spread(status, freq, fill = 0) %>% 
+  mutate(healthy_total = group_overview$total_samples[group_overview$dataset == "original" & 
+                                                        group_overview$status == 'healthy'],
+         sick_total = group_overview$total_samples[group_overview$dataset == "original" & 
+                                                     group_overview$status == 'sick'],
+         healthy_freq = round(healthy / healthy_total, 3),
+         sick_freq = round(sick / sick_total, 3))
+
+genus3 <- its1_genus %>% 
+  filter(dataset == 'extra') %>% 
+  group_by(genus, status) %>% 
+  summarise(freq = n()) %>% 
+  spread(status, freq, fill = 0) %>% 
+  mutate(healthy_total = group_overview$total_samples[group_overview$dataset == "extra" & 
+                                                        group_overview$status == 'healthy'],
+         sick_total = group_overview$total_samples[group_overview$dataset == "extra" & 
+                                                     group_overview$status == 'sick'],
+         healthy_freq = round(healthy / healthy_total, 3),
+         sick_freq = round(sick / sick_total, 3))
+
+### Turn ITS2 into long tables ---------
+its2_long <- its2_raw %>% 
+  select(OTU, header, one_of(samples_its2)) %>% 
+  gather(sample, reads, one_of(samples_its2)) %>% 
+  left_join(sample_info, by = 'sample') %>% 
+  filter(diseased != '-',
+         reads > 1) 
+
+its2_genus <- its2_long %>% 
+  mutate(genus = word(header, end = 1)) %>% 
+  group_by(sample, genus) %>% 
+  mutate(occurence = row_number()) %>% 
+  filter(occurence == 1) %>% 
+  left_join(group_overview[1:3, c(2, 4)], by = 'diseased') %>% 
+  ungroup()
+
+genus2 <- its2_genus %>% 
+  filter(dataset == 'original') %>% 
+  group_by(genus, status) %>% 
+  summarise(freq = n()) %>% 
+  spread(status, freq, fill = 0) %>% 
+  mutate(healthy_total = group_overview$total_samples[group_overview$dataset == "original" & 
+                                                        group_overview$status == 'healthy'],
+         sick_total = group_overview$total_samples[group_overview$dataset == "original" & 
+                                                     group_overview$status == 'sick'],
+         healthy_freq = round(healthy / healthy_total, 3),
+         sick_freq = round(sick / sick_total, 3))
+
+genus4 <- its2_genus %>% 
+  filter(dataset == 'extra') %>% 
+  group_by(genus, status) %>% 
+  summarise(freq = n()) %>% 
+  spread(status, freq, fill = 0) %>% 
+  mutate(healthy_total = group_overview$total_samples[group_overview$dataset == "extra" & 
+                                                        group_overview$status == 'healthy'],
+         sick_total = group_overview$total_samples[group_overview$dataset == "extra" & 
+                                                     group_overview$status == 'sick'],
+         healthy_freq = round(healthy / healthy_total, 3),
+         sick_freq = round(sick / sick_total, 3))
+
+### Output to xlsx files ----------
+library(openxlsx)
+
+l_genus <- list("ITS1_orig" = genus1,
+                "ITS2_orig" = genus2,
+                "ITS1_extra" = genus3,
+                "ITS2_extra" = genus4)
+
+write.xlsx(l_genus, file = "output/new_overview_by_genus.xlsx")
